f9ae42e8d9700c79f8726efc5522574e96b39942
Ahmed Bahaa <ahmedbahaa855@yahoo.com>
2026-06-16 08:00:34 +0000

    Clean order items and payments staging models

--- models/staging/stg_order_payments.sql
+++ models/staging/stg_order_payments.sql
@@ -1,3 +1,7 @@
 select
-    *
+    "order_id" as order_id,
+    "payment_sequential" as payment_sequential,
+    lower("payment_type") as payment_type,
+    "payment_installments" as payment_installments,
+    "payment_value" as payment_value
 from {{ source('olist', 'order_payments') }}
